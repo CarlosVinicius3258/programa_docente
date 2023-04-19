43759425b74368e9798ccf780b79881d9b0cf66e
unknown <Estudante@deinf-00056.deinf.intranet>
2023-04-19 17:24:04 -0300

    add shouldUpdateDocenteRepository test

---
+++ src/test/java/com/ufma/programa_docente/repositories/DocenteRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.ufma.programa_docente.repositories;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ufma.programa_docente.entities.Docente;
+import com.ufma.programa_docente.repositories.DocenteRepository;
+
+@SpringBootTest
+public class DocenteRepositoryTest {
+    @Autowired
+    private DocenteRepository repository;
+
+    @Test
+    public void shouldSaveDocente(){
+        //context
+        Docente docente = Docente.builder().name("César").data(new Date()).build();
+
+        //action
+        Docente saved = repository.save(docente);
+
+        //Verification
+        Assertions.assertNotNull(saved);
+        Assertions.assertEquals(docente.getName(), saved.getName());
+        Assertions.assertEquals(docente.getId_docente(), saved.getId_docente());
+        Assertions.assertEquals(docente.getId_lattes(), saved.getId_lattes());
+        Assertions.assertEquals(docente.getData(), saved.getData());
+        Assertions.assertEquals(docente.getOrientacoes(), saved.getOrientacoes());
+        Assertions.assertEquals(docente.getProducoes(), saved.getProducoes());
+        Assertions.assertEquals(docente.getProgramas(), saved.getProgramas());
+
+    }
+
+    @Test
+    public void shouldUpdateDocente(){
+         //context
+         Docente docente = Docente.builder().name("César").data(new Date()).build();
+
+         //action
+        Docente saved = repository.save(docente);
+        Docente docenteToUpdate = Docente.builder().id_docente(saved.getId_docente()).name("Carlos").data(new Date()).build();
+        Docente updated = repository.save(docenteToUpdate);
+
+        //Verification
+        Assertions.assertNotNull(saved);
+        Assertions.assertNotNull(updated);
+        Assertions.assertEquals(saved.getId_docente(), updated.getId_docente());
+        Assertions.assertNotEquals(saved.getName(), updated.getName());
+
+    }
+    
+}
